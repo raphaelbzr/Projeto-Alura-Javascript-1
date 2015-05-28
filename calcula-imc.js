47b291766eeca555fd32b173f4e18332c8b458bc
raphaelbzr <raphael.bezerra@enken.com.br>
2015-05-28 18:35:21 -0300

    alura

--- calcula-imc.js
+++ calcula-imc.js
@@ -1,16 +1,32 @@
-var peso = document.getElementById("peso-1").textContent; // pega o peso do paciente
-var altura = document.getElementById("altura-1").textContent; // pega a altura do paciente
-var nome = document.getElementById("nome-1").textContent; // pega o nome do paciente
+var nome1 = document.getElementById('nome-1').textContent;
+var peso1 = document.getElementById('peso-1').textContent;
+var altura1 = document.getElementById('altura-1').textContent;
 
-var paciente = {
-    peso : peso,
-    altura : altura,
-    nome : nome
+var paciente1 = {
+	"nome": nome1,
+	"peso": peso1,
+	"altura": altura1
 }
 
-if(paciente.altura != 0){
-    var imc = paciente.peso / (paciente.altura * paciente.altura);
-    console.log(imc);
-}else{
-    console("Não posso executar uma divisão por 0!");
+var nome2 = document.getElementById('nome-2').textContent;
+var peso2 = document.getElementById('peso-2').textContent;
+var altura2 = document.getElementById('altura-2').textContent;
+
+var paciente2 = {
+	"nome": nome2,
+	"peso": peso2,
+	"altura": altura2
+}
+
+var pacientes = [paciente1, paciente2];
+
+for(var posicaoAtual = 0; posicaoAtual <= pacientes.length - 1; posicaoAtual++){
+
+    var pacienteAtual = pacientes[posicaoAtual];
+    if(pacienteAtual.altura != 0){
+        var imcDoPacienteAtual = pacienteAtual.peso / (pacienteAtual.altura * pacienteAtual.altura);
+        console.log(imcDoPacienteAtual) // 25
+    }else{
+        console("Não posso executar uma divisão por 0!");
+    }
 }
