8cd70058c97f6c5af53f42d533e0f8d5a3165dab
raphaelbzr <raphael.bezerra@enken.com.br>
2015-05-28 18:54:01 -0300

    alura

--- calcula-imc.js
+++ calcula-imc.js
@@ -1,32 +1,26 @@
-var nome1 = document.getElementById('nome-1').textContent;
-var peso1 = document.getElementById('peso-1').textContent;
-var altura1 = document.getElementById('altura-1').textContent;
+var nossosTrs = document.getElementsByClassName("paciente");
 
-var paciente1 = {
-	"nome": nome1,
-	"peso": peso1,
-	"altura": altura1
-}
+for(var posicaoDoTrAtual = 0; posicaoDoTrAtual <= nossosTrs.length - 1; posicaoDoTrAtual++){
+    var trAtual = nossosTrs[posicaoDoTrAtual];
+    var nomeTd = trAtual.getElementsByClassName("info-nome")[0]; // pega tr do nome do paciente atual
+    var pesoTd = trAtual.getElementsByClassName("info-peso")[0]; // pega tr do peso do paciente atual
+    var alturaTd = trAtual.getElementsByClassName("info-altura")[0]; // pega tr do altura do paciente atual
+    var imcTd = trAtual.getElementsByClassName("info-imc")[0]; // pega tr do imc do paciente atual
 
-var nome2 = document.getElementById('nome-2').textContent;
-var peso2 = document.getElementById('peso-2').textContent;
-var altura2 = document.getElementById('altura-2').textContent;
+    var paciente = {
+        nome : nomeTd.textContent,
+        peso : pesoTd.textContent,
+        altura : alturaTd.textContent
+    }
 
-var paciente2 = {
-	"nome": nome2,
-	"peso": peso2,
-	"altura": altura2
-}
+    if(paciente.altura != 0){
+        var imcDoPaciente = paciente.peso / (paciente.altura * paciente.altura);
 
-var pacientes = [paciente1, paciente2];
+        imcTd.textContent = imcDoPaciente;
 
-for(var posicaoAtual = 0; posicaoAtual <= pacientes.length - 1; posicaoAtual++){
-
-    var pacienteAtual = pacientes[posicaoAtual];
-    if(pacienteAtual.altura != 0){
-        var imcDoPacienteAtual = pacienteAtual.peso / (pacienteAtual.altura * pacienteAtual.altura);
-        console.log(imcDoPacienteAtual) // 25
-    }else{
+        console.log(imcDoPaciente); // 25
+    } else {
         console("Não posso executar uma divisão por 0!");
     }
+
 }
